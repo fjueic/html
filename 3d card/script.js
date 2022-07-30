6dbed075604978d9f65f5f79f04e7eab47cdd5b3
fjueic <rohitmalik987789@gmail.com>
2022-07-31 00:43:17 +0530

    only card and not responsive

---
+++ 3d card/script.js
@@ -0,0 +1,17 @@
+const sizes=document.querySelectorAll('.size');
+
+
+function removeActiveclasses(){
+    sizes.forEach(size =>{
+        size.classList.remove('active')
+    })
+}
+sizes.forEach(size => {
+    size.addEventListener('click',()=> {
+        removeActiveclasses()
+        size.classList.add('active')
+    })
+});
+
+
+
