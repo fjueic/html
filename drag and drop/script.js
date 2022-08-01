7b2d6ef0d9a5dd565f929ec33ebdd6932b52ba61
fjueic <rohitmalik987789@gmail.com>
2022-08-01 16:17:32 +0530

    drag and drop

---
+++ drag and drop/script.js
@@ -0,0 +1,39 @@
+const fill = document.querySelector(".fill");
+const empties = document.querySelectorAll(".empty");
+
+const dragStart = function () {
+  this.className += " hold";
+  setTimeout(() => (this.className = "invisible"), 0);
+};
+
+const dragEnd = function () {
+  this.className = "fill";
+};
+
+const dragOver = function (e) {
+  e.preventDefault();
+};
+
+const dragEnter = function (e) {
+  e.preventDefault();
+  this.className += " hovered";
+};
+
+const dragLeave = function () {
+  this.className = "empty";
+};
+
+const dragDrop = function () {
+  this.className = "empty";
+  this.append(fill);
+};
+
+fill.addEventListener("dragstart", dragStart);
+fill.addEventListener("dragend", dragEnd);
+
+for (const empty of empties) {
+  empty.addEventListener("dragover", dragOver);
+  empty.addEventListener("dragenter", dragEnter);
+  empty.addEventListener("dragleave", dragLeave);
+  empty.addEventListener("drop", dragDrop);
+}
